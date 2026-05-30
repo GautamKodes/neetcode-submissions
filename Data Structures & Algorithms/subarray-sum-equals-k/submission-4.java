647a9b1d015dc088a0de748ea73c764d3b3be1b8
Gautam Kumar <gautam6605k@proton.me>
2026-05-30 10:00:32 +0530

    Add: subarray-sum-equals-k - submission-4

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-4.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int len = nums.length;
+        int sols = 0;
+        for (int i=0; i<len; i++){
+            int tocheck = nums[i];
+            if (tocheck == k){
+                sols++;
+            } if (i <len-1){
+            for (int j = i+1; j<len; j++){
+                int now = tocheck + nums[j];
+                if (now == k){
+                    sols++;
+                }
+                tocheck = now;
+            }
+            }
+        }
+        return sols;
+    }
+}
