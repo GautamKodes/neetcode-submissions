8217743c5f062e496cfcd8d31c2a792ec0b1d4d9
Gautam Kumar <gautam6605k@proton.me>
2026-06-10 10:19:48 +0530

    Add: three-integer-sum - submission-4

---
+++ Data Structures & Algorithms/three-integer-sum/submission-4.java
@@ -0,0 +1,52 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        int len = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> sols = new ArrayList<>();
+        int i = 0;
+        while (i<=len-3){
+            if (i!=0  && nums[i] == nums[i-1]){
+                i++;
+            } else {
+
+        int right = len-1;
+        int left  = i+1;
+        while (left<right){
+            // System.out.println("i: " + i);
+            // System.out.println("left: " + left);
+            // System.out.println("right: " + right);
+            List<Integer> pair = new ArrayList<>();
+            pair.add(nums[i]);
+            int sum = nums[left] + nums[right] + nums[i];
+            if (sum == 0){
+                pair.add(nums[left]);
+                pair.add(nums[right]);
+                sols.add(pair);
+                left++;
+                while (left < right && nums[left] ==  nums[left-1]){
+                    left++;
+                }
+                right--;
+                while (left < right && nums[right] ==  nums[right+1]){
+                    right--;
+                }
+
+            } else if ( sum > 0){ 
+                right--;
+                while (left < right && nums[right] ==  nums[right+1]){
+                    right--;
+                }
+            } else {
+                left++;
+                while (left < right && nums[left] ==  nums[left-1]){
+                    left++;
+                }
+            }
+        }
+        i++;
+            }
+        }
+        return sols;
+        //-4 -1 -1 0 1 2 len  = 6
+    }
+}
