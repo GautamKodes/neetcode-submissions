9e4a81169a351cec4bf7c043ffac6c35d4190cc4
Gautam Kumar <gautam6605k@proton.me>
2026-06-27 08:20:04 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> s = new Stack<>();
+        int len = temperatures.length;
+        int[] outp = new int[len];
+        int j = 0;
+        int i = 1;
+        int count = 1;
+        int min = temperatures[0];
+        while (j<len-1){
+            if (temperatures[i] > temperatures[j]){
+                outp[j] = count;
+                j++;
+                i = j;
+                count = 0;
+            } else if (i == len-1){
+                outp[j] = 0;
+                j++;
+                i = j;
+                count = 0;
+            }
+            i++;
+            count++;
+        }
+        return outp;
+    }
+}
